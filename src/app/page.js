b4af0be7fb725907a847476e0e0c5f0ab157d458
Anurag <anuragamchi@gmail.com>
2025-04-12 04:11:11 +0530

    made nav items

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Intro from "@/components/Intro";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-black container">
+      <Navbar />
       <div className="container mx-auto px-12 py-4">
         <Intro />
       </div>
